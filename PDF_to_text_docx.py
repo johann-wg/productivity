2bf48f772eb5710d89997d5c58fd8c95ae48ee86
johann-wg <110111343+johann-wg@users.noreply.github.com>
2023-04-17 16:16:14 +0900

    Update PDF_to_text_docx.py

--- PDF_to_text_docx.py
+++ PDF_to_text_docx.py
@@ -2,7 +2,7 @@
 import aspose.words as aw
 
 # PDF 파일 로드
-pdf = aw.Document('/Users/jjong/desktop/test/example.pdf')
+pdf = aw.Document('/Users/desktop/test/example.pdf')
 
 # PDF 텍스트 추출
 text = pdf.to_string(aw.SaveFormat.TEXT)
@@ -13,4 +13,4 @@ para = doc.first_section.body.first_paragraph
 para.append_child(aw.Run(doc, text))
 
 # DOCX 파일로 저장
-doc.save('/Users/jjong/desktop/테스트-테스트.docx')
+doc.save('/Users/desktop/테스트-테스트.docx')
